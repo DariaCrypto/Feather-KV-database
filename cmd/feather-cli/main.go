e830f2502efd60d29ce54a5c432ae1084f34af7a
ddonskaya <dariada.crypton@gmail.com>
2023-09-11 22:23:24 +0530

    Fix cli main function

--- cmd/feather-cli/main.go
+++ cmd/feather-cli/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"log"
 
 	"github.com/ddonskaya/feather/cli"
@@ -14,28 +13,21 @@ var (
 	socket string
 )
 
-func init() {
-	flag.StringVar(&host, "host", "localhost", "host to connect")
-	flag.IntVar(&port, "port", 6869, "port to connect")
-	flag.StringVar(&socket, "socket", "", "listen to unix socket")
-}
-
 func main() {
+
 	var (
-		c   *client.FeatherClient
+		c *client.FeatherClient
 		err error
 	)
-
-	flag.Parse()
-
+	
 	if socket == "" {
 		c, err = cli.ObtainClient(host, port)
 	}
-
 	if err != nil {
-		log.Printf("Error: %s. Exiting.", err.Error())
+		log.Printf("clie: v%", err)
 		return
 	}
 
-	cli.NewTerminal("Start terminal")
+	term := cli.NewTerminal("Start terminal")
+	term.StartSession(c)
 }
