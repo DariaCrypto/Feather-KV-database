835a96185f4781f92adb1833f0ae505263010b94
ddonskaya <dariada.crypton@gmail.com>
2023-09-12 22:56:35 +0530

    change concept. move connPool in server package

--- client/client.go
+++ client/client.go
@@ -8,30 +8,21 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-const (
-	serverAddress = "127.0.0.1:8080"
-	numClients    = 10
-)
-
 type FeatherClient struct {
 	buffer utils.BufferPool
-	connPool  ConnPool
 	MsgHeader []byte
 }
 
 func NewFeatherClient() *FeatherClient {
 	client := &FeatherClient{
 		buffer: *utils.NewBuffer(),
-		connPool: *NewConnectionPool(),
 		MsgHeader: make([]byte, utils.MSG_SIZE),
 	}
 	return client
 }
 
 func PerformCommand(c *FeatherClient, command *protocol.Command) (*protocol.Response, error) {
-	//Get a connection from poolConnection
-	cn, err := c.connPool.Get()
-	defer c.connPool.Put(cn)
+	cn, err := NewConnection()
 	if err != nil {
 		return nil, errors.New("client: Can't got connection from connPool")
 	}
@@ -53,7 +44,7 @@ func PerformCommand(c *FeatherClient, command *protocol.Command) (*protocol.Resp
 	return reply, err
 }
 
-func (c *FeatherClient) getResponse(conn Connection) (*protocol.Response, error){
+func (c *FeatherClient) getResponse(conn *Connection) (*protocol.Response, error){
 	if _, err := utils.ReadData(conn, c.MsgHeader, utils.MSG_SIZE); err != nil {
 		return nil, err
 	}
---
+++ server/pool.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"net"
+	"sync"
+)
+
+const (
+	serverAddress = "127.0.0.1:8080"
+	numClients    = 10
+)
+
+type ConnPool struct {
+	pool chan net.Conn
+	mu   sync.Mutex
+}
+
+func NewConnectionPool() *ConnPool {
+	pool := make(chan net.Conn, numClients)
+	return &ConnPool{
+		pool: pool,
+	}
+}
+
+func (p *ConnPool) Get() (net.Conn, error) {
+	select {
+	case conn := <-p.pool:
+		return conn, nil
+	default:
+		conn, err := net.Dial("tcp", serverAddress)
+		if err != nil {
+			return nil, err
+		}
+		return conn, nil
+	}
+}
+
+func (p *ConnPool) Put(conn net.Conn) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.pool <- conn
+}
